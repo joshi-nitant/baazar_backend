d4bea4165828a205a87d4674cde9826fad16d769
Nitant Joshi <nitantjoshi20@gmail.com>
2020-09-04 02:11:24 +0530

    bug fixex

--- connection.php
+++ connection.php
@@ -1,7 +1,11 @@
 <?php
-$DB_SERVER = '127.0.0.1:3307';
+// $DB_SERVER = '127.0.0.1:3307';
+// $DB_USERNAME = 'root';
+// $DB_PASSWORD = '';
+// $DB_NAME = 'baazar';
+$DB_SERVER = '127.0.0.1';
 $DB_USERNAME = 'root';
-$DB_PASSWORD = '';
+$DB_PASSWORD = 'Demsa@2000';
 $DB_NAME = 'baazar';
 
 $conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
