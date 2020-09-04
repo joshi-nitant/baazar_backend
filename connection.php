528d11eff50da78b5629f3527486fed92345f411
Nitant Joshi <nitantjoshi20@gmail.com>
2020-09-04 16:48:01 +0530

    image reduced

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 // $DB_SERVER = '127.0.0.1:3307';
 // $DB_USERNAME = 'root';
 // $DB_PASSWORD = '';
-// $DB_NAME = 'baazar';
+$DB_NAME = 'baazar';
 $DB_SERVER = '127.0.0.1';
 $DB_USERNAME = 'root';
 $DB_PASSWORD = 'Demsa@2000';
